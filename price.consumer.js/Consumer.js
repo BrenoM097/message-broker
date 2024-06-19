71fbcce56e5ae79167569d8b5ae27eafc5428d68
BrenoM097 <melobreno89@gmail.com>
2024-06-19 13:48:30 -0300

    Criando o consumidor para preco com nodejs

---
+++ price.consumer.js/Consumer.js
@@ -0,0 +1,20 @@
+const amqp = require('amqplib')
+
+const queue = 'PRECO'
+
+amqp.connect({
+    host:'localhost',
+    port: 5672,
+    username: 'admin',
+    password: '12345'
+})
+    .then((connection) => {
+        connection.createChannel()
+        .then((canal) => {
+            canal.consume(fila, (message) => {
+                console.log(message.content.toString())
+            })
+        })
+        .catch((error) => console.log(error))
+    })
+    .catch((error) => console.log(error))
